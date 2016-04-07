d302bf5f28a39aab9d31096c874f80b2f5cf3d66
Humbal Shahi <sh.humble@gmail.com>
2016-04-07 12:55:11 -0400

    added index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-## php file
+
+/** error reporting on  ***/
+error_reporting(E_ALL);
+
+/** error reporting on  ***/
+$site_path = realpath(dirname(__FILE__));
+define('__SITE_PATH', $site_path);
+
+/** include the init.php file **/
+include 'includes/init.php';
+
+/*** load the router ***/
+ $registry->router = new router($registry);
+
+  /*** set the controller path ***/
+ $registry->router->setPath (__SITE_PATH . '/controller');
+
+ /*** load up the template ***/
+ $registry->template = new template($registry);
+
+ /*** load the controller ***/
+ $registry->router->loader();
+
 ?>
